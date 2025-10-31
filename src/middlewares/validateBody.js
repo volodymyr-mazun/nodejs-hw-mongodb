650f0587ac0b5d5fb70eede72670523bf4c16104
Mazun Volodimir <mazunvolodimir777@gmail.com>
2025-10-31 23:34:35 +0200

    correct file validateBody.js

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,15 +3,13 @@ import createHttpError from 'http-errors';
 export function validateBody(schema) {
   return async (req, res, next) => {
     try {
-      await schema.validateAsync(req.body, {
-        abortEarly: false,
-      });
+      await schema.validateAsync(req.body, { abortEarly: false });
       next();
     } catch (err) {
-      const error = createHttpError(400, 'Bad request', {
-        errors: err.details,
-      });
-      next(error);
+      const messages = (err.details || []).map((d) =>
+        d.message.replace(/"/g, ''),
+      );
+      next(createHttpError(400, messages.join(', ')));
     }
   };
 }
